6ca1c2da4ce8fb88b67f58989be330741abe0ae5
Danny575-ui <danushp016@gmail.com>
2025-01-28 15:41:28 +0530

    live preview

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT =  process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname))); // Serve static files (CSS, JS, i
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })//
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
